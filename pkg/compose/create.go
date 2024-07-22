7b94583e2f624045aeb3b112dabaa4c937eb9bf4
Suleiman Dibirov <idsulik@gmail.com>
2024-07-22 19:28:30 +0300

    feat: add separate hashes for service configs and secrets, add file folder support
    Signed-off-by: Suleiman Dibirov <idsulik@gmail.com>

--- pkg/compose/create.go
+++ pkg/compose/create.go
@@ -510,7 +510,12 @@ func (s *composeService) prepareLabels(labels types.Labels, project *types.Proje
 		return nil, err
 	}
 
-	serviceDependenciesHash, err := ServiceDependenciesHash(project, service)
+	serviceConfigsHash, err := ServiceConfigsHash(project, service)
+	if err != nil {
+		return nil, err
+	}
+
+	serviceSecretsHash, err := ServiceSecretsHash(project, service)
 	if err != nil {
 		return nil, err
 	}
@@ -520,7 +525,8 @@ func (s *composeService) prepareLabels(labels types.Labels, project *types.Proje
 		labels[api.ContainerNumberLabel] = strconv.Itoa(number)
 	}
 	labels[api.ConfigHashLabel] = serviceHash
-	labels[api.ConfigHashDependenciesLabel] = serviceDependenciesHash
+	labels[api.ServiceConfigsHash] = serviceConfigsHash
+	labels[api.ServiceSecretsHash] = serviceSecretsHash
 	labels[api.ContainerNumberLabel] = strconv.Itoa(number)
 
 	var dependencies []string
